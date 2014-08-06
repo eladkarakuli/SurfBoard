564cf35619b68702c6264ead8276ea1c15e0d1dc
Elad Karakuli <elad.karakuli@gmail.com>
2014-08-06 23:39:33 +0300

    Updating npm and bower references, Add social-cms-backend liberry and update server middleware

--- server/app.js
+++ server/app.js
@@ -10,15 +10,26 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
+var SCB = require('social-cms-backend');
 
 // Connect to database
 /*mongoose.connect(config.mongo.uri, config.mongo.options);*/
 
 // Populate DB with sample data
-if(config.seedDB) { require('./config/seed'); }
+//if(config.seedDB) { require('./config/seed'); }
 
 // Setup server
 var app = express();
+
+// Set social cms backend
+app.use(SCB.middleware({
+  mongodb_url: 'mongodb://localhost:27017/socialcmsdb',
+  // Disable facebook connection
+  /*passport_strategy: 'facebook',
+  facebook_app_id: process.env.FACEBOOK_APP_ID,
+  facebook_app_secret: process.env.FACEBOOK_APP_SECRET*/
+}));
+
 var server = require('http').createServer(app);
 require('./config/express')(app);
 require('./routes')(app);
